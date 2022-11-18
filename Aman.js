ecbdb53f161746f74711d62cb0bf275180b112bc
Aman21591 <yadavaman21591@gmail.com>
2022-11-18 16:33:35 +0530

    add title

---
+++ Aman.js
@@ -0,0 +1,15 @@
+var isDirty = function() { return false; }
+
+window.onload = function() {
+    window.addEventListener("beforeunload", function (e) {
+        if (formSubmitting || !isDirty()) {
+            return undefined;
+        }
+        
+        var confirmationMessage = 'It looks like you have been editing something. '
+                                + 'If you leave before saving, your changes will be lost.';
+
+        (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+        return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+    });
+};
